0de1bf98c2042b7f137e81faf9ec6bc9360256c9
slayway <Relocation1337@gmail.com>
2018-09-24 20:56:42 +0300

    create splash activity, no tittle, no action bar

--- app/src/main/java/com/slayway/game/MainActivity.java
+++ app/src/main/java/com/slayway/game/MainActivity.java
@@ -1,13 +1,19 @@
 package com.slayway.game;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
 
